3f86c7bbd322bcd0bef7f9063b294df31f0f5c55
Martin Oywa <martinoywa2@gmail.com>
2024-05-14 18:09:25 +0300

    Add Postgres database

--- src/db/sqlite.py
+++ src/db/sqlite.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./feathr.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
